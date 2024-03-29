71e0c5a8ea716f064d39d00149f35d733dc3d3fa
erfannvb <erfannavab@gmail.com>
2024-03-29 17:34:10 +0330

    created audit aware

---
+++ src/main/java/nvb/dev/springadvancedproject/auth/AppAuditorAware.java
@@ -0,0 +1,26 @@
+package nvb.dev.springadvancedproject.auth;
+
+import nvb.dev.springadvancedproject.model.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AppAuditorAware implements AuditorAware<String> {
+
+    @Override
+    @NonNull
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() || authentication instanceof AnonymousAuthenticationToken) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getUsername());
+    }
+}
