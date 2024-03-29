16d7802b849f97152b046a413b44588075860914
erfannvb <erfannavab@gmail.com>
2024-03-29 17:36:17 +0330

    jpaAuditing refers to the auditorAware bean

--- src/main/java/nvb/dev/springadvancedproject/SpringAdvancedProjectApplication.java
+++ src/main/java/nvb/dev/springadvancedproject/SpringAdvancedProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableCaching
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SpringAdvancedProjectApplication {
 
     public static void main(String[] args) {
