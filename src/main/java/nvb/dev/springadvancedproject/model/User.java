75ded72c12f78e40834f6039e279c09c635e98ee
erfannvb <erfannavab@gmail.com>
2024-03-29 16:26:34 +0330

    created User entity

---
+++ src/main/java/nvb/dev/springadvancedproject/model/User.java
@@ -0,0 +1,88 @@
+package nvb.dev.springadvancedproject.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "firstName cannot be empty.")
+    @NotBlank(message = "firstName cannot be blank.")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @NotEmpty(message = "lastName cannot be empty.")
+    @NotBlank(message = "lastName cannot be blank.")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotEmpty(message = "username cannot be empty.")
+    @NotBlank(message = "username cannot be blank.")
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @NotEmpty(message = "password cannot be empty.")
+    @NotBlank(message = "password cannot be blank.")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
