f96c4e29b2ec41e6da8a9e004ff3c8118aeb1d1d
erfannvb <erfannavab@gmail.com>
2024-03-29 17:33:47 +0330

    redis cache manager is configured

---
+++ src/main/java/nvb/dev/springadvancedproject/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package nvb.dev.springadvancedproject.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(redisHost);
+        configuration.setPort(redisPort);
+        return new JedisConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(JedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.create(connectionFactory);
+    }
+
+}
