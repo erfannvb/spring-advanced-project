086cf6539db02135c6d78838b1558b84c79fecdd
erfannvb <erfannavab@gmail.com>
2024-03-29 18:32:35 +0330

    created loan entity

---
+++ src/main/java/nvb/dev/springadvancedproject/model/Loan.java
@@ -0,0 +1,43 @@
+package nvb.dev.springadvancedproject.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "tbl_loans")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Loan extends AuditEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "loan_date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate loanDate;
+
+    @Column(name = "due_date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate dueDate;
+
+    @Column(name = "return_date")
+    @Temporal(TemporalType.DATE)
+    private LocalDate returnDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+}
