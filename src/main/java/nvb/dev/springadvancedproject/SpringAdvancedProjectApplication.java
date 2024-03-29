1272574a1a37c004db72dc3ac77e3aab60680045
erfannvb <erfannavab@gmail.com>
2024-03-29 15:52:36 +0330

    Initial commit

---
+++ src/main/java/nvb/dev/springadvancedproject/SpringAdvancedProjectApplication.java
@@ -0,0 +1,13 @@
+package nvb.dev.springadvancedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nvb/dev/springadvancedproject/SpringAdvancedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.dev.springadvancedproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
