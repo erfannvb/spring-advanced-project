1058bc51e2ecc4c05573548bb1deae43c6b2fdb9
erfannvb <erfannavab@gmail.com>
2024-03-30 20:26:49 +0330

    removed unwanted APIs

--- src/main/java/nvb/dev/springadvancedproject/controller/UserController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/UserController.java
@@ -1,7 +1,16 @@
 package nvb.dev.springadvancedproject.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import nvb.dev.springadvancedproject.dto.UserDto;
+import nvb.dev.springadvancedproject.mapper.UserMapper;
+import nvb.dev.springadvancedproject.model.UserEntity;
+import nvb.dev.springadvancedproject.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class UserController {
 
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @PostMapping(path = "/save")
+    public ResponseEntity<UserDto> saveUser(@RequestBody @Valid UserDto userDto) {
+        UserEntity userEntity = userMapper.mapToUserEntity(userDto);
+        UserEntity savedUser = userService.saveUser(userEntity);
+        return new ResponseEntity<>(userMapper.mapToUserDto(savedUser), HttpStatus.CREATED);
+    }
 
 }
