3638335e6e59c04f5c74ca74be9f108f1cd36556
erfannvb <erfannavab@gmail.com>
2024-03-30 14:56:52 +0330

    exception messages are here

---
+++ src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
@@ -0,0 +1,10 @@
+package nvb.dev.springadvancedproject.constant;
+
+public class ExceptionMessage {
+
+    private ExceptionMessage() {
+    }
+
+    public static final String USERNAME_NOT_FOUND = "Username '%s' does not exist.";
+
+}
