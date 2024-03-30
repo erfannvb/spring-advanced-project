cfe7fd5ab974373d0741ea4dd1504cc24e8aa0d9
erfannvb <erfannavab@gmail.com>
2024-03-30 14:57:43 +0330

    controller layer is created

---
+++ src/main/java/nvb/dev/springadvancedproject/controller/UserController.java
@@ -0,0 +1,15 @@
+package nvb.dev.springadvancedproject.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/user")
+@RequiredArgsConstructor
+@Validated
+public class UserController {
+
+
+}
