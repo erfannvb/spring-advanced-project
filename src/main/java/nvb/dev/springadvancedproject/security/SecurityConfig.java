9e6675009486f192c17e65190e5a28bdab69a89f
erfannvb <erfannavab@gmail.com>
2024-03-30 14:57:12 +0330

    did some security stuff

---
+++ src/main/java/nvb/dev/springadvancedproject/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package nvb.dev.springadvancedproject.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static nvb.dev.springadvancedproject.constant.Constant.USER_URL;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(USER_URL).permitAll()
+                );
+
+        return httpSecurity.build();
+    }
+
+}
