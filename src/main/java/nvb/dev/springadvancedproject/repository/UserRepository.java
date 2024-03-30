5650fa46b5a048ff9cb9ca1dec84c737101abc94
erfannvb <erfannavab@gmail.com>
2024-03-30 05:55:57 +0330

    added findByUsername method

--- src/main/java/nvb/dev/springadvancedproject/repository/UserRepository.java
+++ src/main/java/nvb/dev/springadvancedproject/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package nvb.dev.springadvancedproject.repository;
 
-import nvb.dev.springadvancedproject.model.UserModel;
+import nvb.dev.springadvancedproject.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+
 }
