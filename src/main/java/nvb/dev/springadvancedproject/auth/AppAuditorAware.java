eb9711e9cbfb9618839853871bcc88db021f3c7d
erfannvb <erfannavab@gmail.com>
2024-03-30 05:55:31 +0330

    renamed UserModel to UserEntity

--- src/main/java/nvb/dev/springadvancedproject/auth/AppAuditorAware.java
+++ src/main/java/nvb/dev/springadvancedproject/auth/AppAuditorAware.java
@@ -1,6 +1,6 @@
 package nvb.dev.springadvancedproject.auth;
 
-import nvb.dev.springadvancedproject.model.UserModel;
+import nvb.dev.springadvancedproject.model.UserEntity;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -20,7 +20,7 @@ public class AppAuditorAware implements AuditorAware<String> {
             return Optional.empty();
         }
 
-        UserModel userModelPrincipal = (UserModel) authentication.getPrincipal();
-        return Optional.ofNullable(userModelPrincipal.getUsername());
+        UserEntity userEntityPrincipal = (UserEntity) authentication.getPrincipal();
+        return Optional.ofNullable(userEntityPrincipal.getUsername());
     }
 }
