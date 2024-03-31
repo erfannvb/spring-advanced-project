41a1a1334debf095ce5b665de1bc16c17374566f
erfannvb <erfannavab@gmail.com>
2024-03-31 15:24:46 +0330

    - auth url is added - token-related vars are added

--- src/main/java/nvb/dev/springadvancedproject/constant/Constant.java
+++ src/main/java/nvb/dev/springadvancedproject/constant/Constant.java
@@ -6,5 +6,12 @@ public class Constant {
     }
 
     public static final String USER_URL = "/api/user/**";
+    public static final String AUTH_URL = "/api/auth/**";
+
+    public static final int TOKEN_EXPIRATION_TIME = 1000 * 60 * 24;
+    public static final int REFRESH_TOKEN_EXPIRATION_TIME = 604800000;
+
+    public static final String BEARER = "Bearer ";
+    public static final String SECRET_KEY = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
 }
