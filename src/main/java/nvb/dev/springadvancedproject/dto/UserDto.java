3814c55f237da2469b769bf5f52a412e97ef742a
erfannvb <erfannavab@gmail.com>
2024-03-31 15:47:16 +0330

    added constraints to the fields

--- src/main/java/nvb/dev/springadvancedproject/dto/UserDto.java
+++ src/main/java/nvb/dev/springadvancedproject/dto/UserDto.java
@@ -1,5 +1,11 @@
 package nvb.dev.springadvancedproject.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import nvb.dev.springadvancedproject.model.BookEntity;
 import nvb.dev.springadvancedproject.model.UserRole;
@@ -10,12 +16,32 @@ import java.util.Set;
 public class UserDto {
 
     private String id;
+
+    @NotNull(message = "firstName cannot be null.")
+    @NotBlank(message = "firstName cannot be blank.")
     private String firstName;
+
+    @NotNull(message = "lastName cannot be null.")
+    @NotBlank(message = "lastName cannot be blank.")
     private String lastName;
+
+    @NotNull(message = "username cannot be null.")
+    @NotBlank(message = "username cannot be blank.")
     private String username;
+
+    @NotNull(message = "email cannot be null.")
+    @NotBlank(message = "email cannot be blank.")
+    @Email
     private String email;
+
+    @NotNull(message = "password cannot be null.")
+    @NotBlank(message = "password cannot be blank.")
+    @Size(min = 3, message = "password must at least contain 3 characters.")
     private String password;
+
+    @Enumerated(EnumType.STRING)
     private UserRole userRole;
+
     private Set<BookEntity> books;
 
 }
