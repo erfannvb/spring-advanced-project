d32789276a481e36aa24af59dbbf9a8598224133
erfannvb <erfannavab@gmail.com>
2024-03-31 15:24:08 +0330

    user not found message is added

--- src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
+++ src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
@@ -7,6 +7,5 @@ public class ExceptionMessage {
 
     public static final String USERNAME_NOT_FOUND = "Username '%s' does not exist.";
     public static final String USER_NOT_FOUND = "User not found.";
-    public static final String NO_USERS_FOUND = "There is no user on the list.";
 
 }
