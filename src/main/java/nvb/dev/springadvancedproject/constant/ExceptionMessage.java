1ee2d03d1eb51f763fa7fcbef70a3e6a17370136
erfannvb <erfannavab@gmail.com>
2024-03-31 23:47:55 +0330

    added a new message

--- src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
+++ src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
@@ -7,6 +7,9 @@ public class ExceptionMessage {
 
     public static final String USERNAME_NOT_FOUND = "Username '%s' does not exist.";
     public static final String USER_NOT_FOUND = "User not found.";
+
     public static final String ENTITY_NOT_STORABLE = "There was an error in storing the data.";
+
+    public static final String AUTHOR_NOT_FOUND = "Author not found.";
 
 }
