7cd976c66e7a11cf0f8a4e1ab7e6485806eaa44b
erfannvb <erfannavab@gmail.com>
2024-03-31 15:29:32 +0330

    - injected required beans - added authenticationProvider and authenticationManager beans - added more security configs

--- src/main/java/nvb/dev/springadvancedproject/security/SecurityConfig.java
+++ src/main/java/nvb/dev/springadvancedproject/security/SecurityConfig.java
@@ -1,13 +1,23 @@
 package nvb.dev.springadvancedproject.security;
 
 import lombok.RequiredArgsConstructor;
+import nvb.dev.springadvancedproject.security.filter.JwtAuthenticationFilter;
+import nvb.dev.springadvancedproject.security.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static nvb.dev.springadvancedproject.constant.Constant.AUTH_URL;
 import static nvb.dev.springadvancedproject.constant.Constant.USER_URL;
 
 @Configuration
@@ -15,15 +25,36 @@ import static nvb.dev.springadvancedproject.constant.Constant.USER_URL;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(USER_URL).permitAll()
-                );
+                        .requestMatchers(AUTH_URL).permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 }
