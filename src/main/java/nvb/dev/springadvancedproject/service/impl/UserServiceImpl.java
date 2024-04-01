35f3e91db2f271813c4a96064b2ad1f704e02de2
erfannvb <erfannavab@gmail.com>
2024-04-02 03:05:31 +0330

    implemented usernameExists method and used it in the saveUser method

--- src/main/java/nvb/dev/springadvancedproject/service/impl/UserServiceImpl.java
+++ src/main/java/nvb/dev/springadvancedproject/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package nvb.dev.springadvancedproject.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import nvb.dev.springadvancedproject.exception.UsernameExistsException;
 import nvb.dev.springadvancedproject.model.UserEntity;
 import nvb.dev.springadvancedproject.repository.UserRepository;
 import nvb.dev.springadvancedproject.service.UserService;
@@ -16,7 +17,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserEntity saveUser(UserEntity userEntity) {
+        if (usernameExists(userEntity.getUsername())) throw new UsernameExistsException(userEntity.getUsername());
         userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
         return userRepository.save(userEntity);
     }
+
+    private boolean usernameExists(String username) {
+        return userRepository.findByUsername(username).isPresent();
+    }
+
 }
