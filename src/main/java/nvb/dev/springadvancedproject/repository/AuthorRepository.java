2854bac3419a2b585f6d9f49707a23edeaf9372b
erfannvb <erfannavab@gmail.com>
2024-04-01 15:40:34 +0330

    added two custom methods

--- src/main/java/nvb/dev/springadvancedproject/repository/AuthorRepository.java
+++ src/main/java/nvb/dev/springadvancedproject/repository/AuthorRepository.java
@@ -2,8 +2,18 @@ package nvb.dev.springadvancedproject.repository;
 
 import nvb.dev.springadvancedproject.model.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
+
+    Optional<AuthorEntity> findByFirstNameIgnoreCase(String firstName);
+
+    @Query("SELECT a FROM AuthorEntity a WHERE a.age BETWEEN :minAge AND :maxAge")
+    List<AuthorEntity> findByAgeBetween(int minAge, int maxAge);
+
 }
