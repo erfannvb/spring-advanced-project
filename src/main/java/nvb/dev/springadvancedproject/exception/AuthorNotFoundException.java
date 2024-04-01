c20c6d09db69fa1c81614271b2b789924664fe37
erfannvb <erfannavab@gmail.com>
2024-04-02 03:02:36 +0330

    - added error message constant - added a new constructor

--- src/main/java/nvb/dev/springadvancedproject/exception/AuthorNotFoundException.java
+++ src/main/java/nvb/dev/springadvancedproject/exception/AuthorNotFoundException.java
@@ -8,7 +8,13 @@ import static nvb.dev.springadvancedproject.constant.ExceptionMessage.AUTHOR_NOT
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class AuthorNotFoundException extends RuntimeException {
 
+    private static final String ERROR_MESSAGE = "Author with id '%d' does not exist.";
+
     public AuthorNotFoundException() {
         super(AUTHOR_NOT_FOUND);
+    }
+
+    public AuthorNotFoundException(Long id) {
+        super(ERROR_MESSAGE.formatted(id));
     }
 }
