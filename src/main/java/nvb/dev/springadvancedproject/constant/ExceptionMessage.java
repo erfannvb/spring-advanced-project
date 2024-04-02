2b5b6397511652fd1eff6c2314744f635abfc1e1
erfannvb <erfannavab@gmail.com>
2024-04-02 03:40:59 +0330

    book related messages are added

--- src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
+++ src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
@@ -13,4 +13,7 @@ public class ExceptionMessage {
     public static final String AUTHOR_NOT_FOUND = "Author not found.";
     public static final String NO_AUTHORS_FOUND = "There is no author in the database.";
 
+    public static final String BOOK_NOT_FOUND = "Book not found.";
+    public static final String NO_BOOKS_FOUND = "There is no book in the database.";
+
 }
