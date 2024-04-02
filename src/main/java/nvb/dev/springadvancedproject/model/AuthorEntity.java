67a42e5844c59d404d74970afbe4dfa45a55f19a
erfannvb <erfannavab@gmail.com>
2024-04-02 20:59:24 +0330

    optimized the imports

--- src/main/java/nvb/dev/springadvancedproject/model/AuthorEntity.java
+++ src/main/java/nvb/dev/springadvancedproject/model/AuthorEntity.java
@@ -9,9 +9,7 @@ import lombok.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
