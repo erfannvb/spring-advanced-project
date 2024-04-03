4737775a6a7b5394dc88570856141a75f46dc6ad
erfannvb <erfannavab@gmail.com>
2024-04-03 14:10:00 +0330

    - removed some of the messages - added a new member-related message

--- src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
+++ src/main/java/nvb/dev/springadvancedproject/constant/ExceptionMessage.java
@@ -10,10 +10,10 @@ public class ExceptionMessage {
 
     public static final String ENTITY_NOT_STORABLE = "There was an error in storing the data.";
 
-    public static final String AUTHOR_NOT_FOUND = "Author not found.";
     public static final String NO_AUTHORS_FOUND = "There is no author in the database.";
 
-    public static final String BOOK_NOT_FOUND = "Book not found.";
     public static final String NO_BOOKS_FOUND = "There is no book in the database.";
+
+    public static final String NO_MEMBERS_FOUND = "There is no member in the database";
 
 }
