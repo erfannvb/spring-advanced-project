d427ea4930f18e574d69997db75f6462dce12ae2
erfannvb <erfannavab@gmail.com>
2024-04-03 23:23:31 +0330

    added delete method

--- src/main/java/nvb/dev/springadvancedproject/repository/LoanRepository.java
+++ src/main/java/nvb/dev/springadvancedproject/repository/LoanRepository.java
@@ -3,7 +3,12 @@ package nvb.dev.springadvancedproject.repository;
 import nvb.dev.springadvancedproject.model.LoanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
+
+    @Transactional
+    void deleteByMemberIdAndBookId(long memberId, long bookId);
+
 }
