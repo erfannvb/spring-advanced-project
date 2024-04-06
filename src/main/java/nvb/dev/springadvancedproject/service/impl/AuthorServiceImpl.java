76dd782107a0d825a9e68aaec42a780da5fca60c
erfannvb <erfannavab@gmail.com>
2024-04-06 23:24:56 +0330

    removed the line : assert field != null

--- src/main/java/nvb/dev/springadvancedproject/service/impl/AuthorServiceImpl.java
+++ src/main/java/nvb/dev/springadvancedproject/service/impl/AuthorServiceImpl.java
@@ -97,7 +97,6 @@ public class AuthorServiceImpl implements AuthorService {
         if (foundAuthor.isPresent()) {
             authorEntity.forEach((key, value) -> {
                 Field field = ReflectionUtils.findField(AuthorEntity.class, key);
-                assert field != null;
                 field.setAccessible(true);
                 ReflectionUtils.setField(field, foundAuthor.get(), value);
             });
