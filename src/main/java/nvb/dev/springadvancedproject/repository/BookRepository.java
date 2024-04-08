229855bab9649e87584670affeac6b75ce0b0d5b
erfannvb <erfannavab@gmail.com>
2024-04-08 18:08:04 +0330

    added custom query

--- src/main/java/nvb/dev/springadvancedproject/repository/BookRepository.java
+++ src/main/java/nvb/dev/springadvancedproject/repository/BookRepository.java
@@ -2,6 +2,7 @@ package nvb.dev.springadvancedproject.repository;
 
 import nvb.dev.springadvancedproject.model.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
+    @Query("SELECT b FROM BookEntity b WHERE b.author.id = :authorId")
     List<BookEntity> findBookByAuthorId(long authorId);
 
     @Transactional
