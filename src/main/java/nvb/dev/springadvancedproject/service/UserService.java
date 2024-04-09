60ac387a3af93605dafd243b90bb344f6b495926
erfannvb <erfannavab@gmail.com>
2024-04-09 16:44:45 +0330

    implemented changePassword method

--- src/main/java/nvb/dev/springadvancedproject/service/UserService.java
+++ src/main/java/nvb/dev/springadvancedproject/service/UserService.java
@@ -1,9 +1,12 @@
 package nvb.dev.springadvancedproject.service;
 
+import nvb.dev.springadvancedproject.dto.request.ChangePasswordRequest;
 import nvb.dev.springadvancedproject.model.UserEntity;
 
 public interface UserService {
 
     UserEntity saveUser(UserEntity userEntity);
+
+    void changePassword(String userId, ChangePasswordRequest request);
 
 }
--- src/main/java/nvb/dev/springadvancedproject/service/impl/UserServiceImpl.java
+++ src/main/java/nvb/dev/springadvancedproject/service/impl/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package nvb.dev.springadvancedproject.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import nvb.dev.springadvancedproject.dto.request.ChangePasswordRequest;
 import nvb.dev.springadvancedproject.exception.user.UsernameExistsException;
 import nvb.dev.springadvancedproject.model.UserEntity;
 import nvb.dev.springadvancedproject.repository.UserRepository;
 import nvb.dev.springadvancedproject.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,27 @@ public class UserServiceImpl implements UserService {
         if (usernameExists(userEntity.getUsername())) throw new UsernameExistsException(userEntity.getUsername());
         userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
         return userRepository.save(userEntity);
+    }
+
+    @Override
+    public void changePassword(String userId, ChangePasswordRequest request) {
+        Optional<UserEntity> foundUser = userRepository.findById(userId);
+        if (foundUser.isPresent()) {
+
+            UserEntity userEntity = foundUser.get();
+
+            if (!passwordEncoder.matches(request.getOldPassword(), userEntity.getPassword())) {
+                throw new IllegalArgumentException("Wrong password");
+            }
+
+            if (!request.getNewPassword().equals(request.getConfirmPassword())) {
+                throw new IllegalArgumentException("Passwords do not match");
+            }
+
+            userEntity.setPassword(passwordEncoder.encode(request.getNewPassword()));
+            userRepository.save(userEntity);
+
+        }
     }
 
     private boolean usernameExists(String username) {
