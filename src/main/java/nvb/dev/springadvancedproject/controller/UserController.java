9e24cebc3f457fec0ea17df4c5909e4cca6905c4
erfannvb <erfannavab@gmail.com>
2024-04-09 16:45:01 +0330

    created changePassword API

--- src/main/java/nvb/dev/springadvancedproject/controller/UserController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/UserController.java
@@ -3,6 +3,7 @@ package nvb.dev.springadvancedproject.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import nvb.dev.springadvancedproject.dto.UserDto;
+import nvb.dev.springadvancedproject.dto.request.ChangePasswordRequest;
 import nvb.dev.springadvancedproject.mapper.UserMapper;
 import nvb.dev.springadvancedproject.model.UserEntity;
 import nvb.dev.springadvancedproject.service.UserService;
@@ -10,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/user")
@@ -31,6 +29,12 @@ public class UserController {
         UserEntity userEntity = userMapper.toUserEntity(userDto);
         UserEntity savedUser = userService.saveUser(userEntity);
         return new ResponseEntity<>(userMapper.toUserDto(savedUser), HttpStatus.CREATED);
+    }
+
+    @PatchMapping(path = "/{userId}")
+    public ResponseEntity<Void> changePassword(@PathVariable String userId, @RequestBody ChangePasswordRequest request) {
+        userService.changePassword(userId, request);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
