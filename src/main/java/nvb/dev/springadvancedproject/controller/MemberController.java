c86d825df8223230d501656f734d7c416f3e8d27
erfannvb <erfannavab@gmail.com>
2024-04-10 06:24:38 +0330

    integrated HATEOAS into the APIs

--- src/main/java/nvb/dev/springadvancedproject/controller/MemberController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/MemberController.java
@@ -2,10 +2,13 @@ package nvb.dev.springadvancedproject.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import nvb.dev.springadvancedproject.assembler.MemberModelAssembler;
 import nvb.dev.springadvancedproject.dto.MemberDto;
 import nvb.dev.springadvancedproject.mapper.MemberMapper;
 import nvb.dev.springadvancedproject.model.MemberEntity;
 import nvb.dev.springadvancedproject.service.MemberService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,51 +26,56 @@ public class MemberController {
 
     private final MemberService memberService;
     private final MemberMapper memberMapper;
+    private final MemberModelAssembler memberModelAssembler;
 
     @PostMapping(path = "/save")
-    public ResponseEntity<MemberDto> saveMember(@RequestBody @Valid MemberDto memberDto) {
+    public ResponseEntity<EntityModel<MemberDto>> saveMember(@RequestBody @Valid MemberDto memberDto) {
         MemberEntity memberEntity = memberMapper.toMemberEntity(memberDto);
         MemberEntity savedMember = memberService.saveMember(memberEntity);
-        return new ResponseEntity<>(memberMapper.toMemberDto(savedMember), HttpStatus.CREATED);
+        EntityModel<MemberDto> model = memberModelAssembler.toModel(savedMember);
+        return new ResponseEntity<>(model, HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/{memberId}")
-    public ResponseEntity<MemberDto> getMemberById(@PathVariable long memberId) {
+    public ResponseEntity<EntityModel<MemberDto>> getMemberById(@PathVariable long memberId) {
         Optional<MemberEntity> foundMember = memberService.getMemberById(memberId);
-        return foundMember.map(memberEntity -> {
-            MemberDto memberDto = memberMapper.toMemberDto(memberEntity);
-            return new ResponseEntity<>(memberDto, HttpStatus.OK);
-        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return foundMember.map(memberModelAssembler::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping
-    public ResponseEntity<MemberDto> getMemberByFullName(@RequestParam String fullName) {
+    public ResponseEntity<EntityModel<MemberDto>> getMemberByFullName(@RequestParam String fullName) {
         Optional<MemberEntity> memberByFullName = memberService.getMemberByFullName(fullName);
-        return memberByFullName.map(memberEntity -> {
-            MemberDto memberDto = memberMapper.toMemberDto(memberEntity);
-            return new ResponseEntity<>(memberDto, HttpStatus.OK);
-        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return memberByFullName.map(memberModelAssembler::toModel)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping(path = "/all")
-    public ResponseEntity<List<MemberDto>> getAllMembers() {
+    public ResponseEntity<CollectionModel<EntityModel<MemberDto>>> getAllMembers() {
         List<MemberEntity> allMembers = memberService.getAllMembers();
-        List<MemberDto> memberDtoList = allMembers.stream().map(memberMapper::toMemberDto).toList();
-        return new ResponseEntity<>(memberDtoList, HttpStatus.OK);
+        if (allMembers.isEmpty()) return ResponseEntity.notFound().build();
+
+        CollectionModel<EntityModel<MemberDto>> collectionModel = memberModelAssembler.toCollectionModel(allMembers);
+
+        return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
     @PutMapping(path = "/{memberId}")
-    public ResponseEntity<MemberDto> updateMember(@PathVariable long memberId, @RequestBody @Valid MemberDto memberDto) {
+    public ResponseEntity<EntityModel<MemberDto>> updateMember(@PathVariable long memberId, @RequestBody @Valid MemberDto memberDto) {
         MemberEntity memberEntity = memberMapper.toMemberEntity(memberDto);
         MemberEntity updatedMember = memberService.updateMember(memberId, memberEntity);
-        return new ResponseEntity<>(memberMapper.toMemberDto(updatedMember), HttpStatus.OK);
+        EntityModel<MemberDto> model = memberModelAssembler.toModel(updatedMember);
+        return new ResponseEntity<>(model, HttpStatus.OK);
     }
 
     @PatchMapping(path = "/{memberId}")
-    public ResponseEntity<MemberDto> partialUpdate(@PathVariable long memberId,
-                                                   @RequestBody @Valid Map<String, Object> memberDto) {
+    public ResponseEntity<EntityModel<MemberDto>> partialUpdate(@PathVariable long memberId,
+                                                                @RequestBody @Valid Map<String, Object> memberDto) {
         MemberEntity memberEntity = memberService.partialUpdate(memberId, memberDto);
-        return new ResponseEntity<>(memberMapper.toMemberDto(memberEntity), HttpStatus.OK);
+        EntityModel<MemberDto> model = memberModelAssembler.toModel(memberEntity);
+        return new ResponseEntity<>(model, HttpStatus.OK);
     }
 
     @DeleteMapping(path = "/{memberId}")
