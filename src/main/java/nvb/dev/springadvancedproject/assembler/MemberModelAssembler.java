4409a6696497cfa604e424bae7865cb93a3ae6ad
erfannvb <erfannavab@gmail.com>
2024-04-10 05:43:04 +0330

    implemented member model assembler to assemble EntityModel for MemberEntity

---
+++ src/main/java/nvb/dev/springadvancedproject/assembler/MemberModelAssembler.java
@@ -0,0 +1,27 @@
+package nvb.dev.springadvancedproject.assembler;
+
+import nvb.dev.springadvancedproject.controller.MemberController;
+import nvb.dev.springadvancedproject.model.MemberEntity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Configuration
+public class MemberModelAssembler implements RepresentationModelAssembler<MemberEntity, EntityModel<MemberEntity>> {
+
+    @Override
+    @NonNull
+    public EntityModel<MemberEntity> toModel(@NonNull MemberEntity member) {
+        EntityModel<MemberEntity> memberModel = EntityModel.of(member);
+
+        memberModel.add(linkTo(methodOn(MemberController.class).getMemberById(member.getId())).withSelfRel());
+        memberModel.add(linkTo(methodOn(MemberController.class).getAllMembers()).withRel(IanaLinkRelations.COLLECTION));
+
+        return memberModel;
+    }
+}
