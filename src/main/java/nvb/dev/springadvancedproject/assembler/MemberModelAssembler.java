c1bf0cad7753618788d1b56eff6c90a027be6f47
erfannvb <erfannavab@gmail.com>
2024-04-10 05:58:51 +0330

    - changed the annotations - injected MemberMapper - fixed the implementation

--- src/main/java/nvb/dev/springadvancedproject/assembler/MemberModelAssembler.java
+++ src/main/java/nvb/dev/springadvancedproject/assembler/MemberModelAssembler.java
@@ -1,24 +1,31 @@
 package nvb.dev.springadvancedproject.assembler;
 
+import lombok.RequiredArgsConstructor;
 import nvb.dev.springadvancedproject.controller.MemberController;
+import nvb.dev.springadvancedproject.dto.MemberDto;
+import nvb.dev.springadvancedproject.mapper.MemberMapper;
 import nvb.dev.springadvancedproject.model.MemberEntity;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-@Configuration
-public class MemberModelAssembler implements RepresentationModelAssembler<MemberEntity, EntityModel<MemberEntity>> {
+@Component
+@RequiredArgsConstructor
+public class MemberModelAssembler implements RepresentationModelAssembler<MemberEntity, EntityModel<MemberDto>> {
+
+    private final MemberMapper memberMapper;
 
     @Override
     @NonNull
-    public EntityModel<MemberEntity> toModel(@NonNull MemberEntity member) {
-        return EntityModel.of(member,
-                linkTo(methodOn(MemberController.class).getMemberById(member.getId())).withSelfRel(),
+    public EntityModel<MemberDto> toModel(@NonNull MemberEntity memberEntity) {
+        MemberDto memberDto = memberMapper.toMemberDto(memberEntity);
+        return EntityModel.of(memberDto,
+                linkTo(methodOn(MemberController.class).getMemberById(memberDto.getId())).withSelfRel(),
                 linkTo(methodOn(MemberController.class).getAllMembers()).withRel(IanaLinkRelations.COLLECTION));
     }
 }
