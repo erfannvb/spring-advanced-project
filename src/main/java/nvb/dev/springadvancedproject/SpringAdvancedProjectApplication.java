f5a3f61d732e88f4e9d8388f30f298ca6d3a9d88
erfannvb <erfannavab@gmail.com>
2024-04-12 08:48:06 +0330

    enabled aspectJ

--- src/main/java/nvb/dev/springadvancedproject/SpringAdvancedProjectApplication.java
+++ src/main/java/nvb/dev/springadvancedproject/SpringAdvancedProjectApplication.java
@@ -3,11 +3,13 @@ package nvb.dev.springadvancedproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableCaching
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class SpringAdvancedProjectApplication {
 
     public static void main(String[] args) {
