be7f8c7a22fb4acaa12d59a2204287ab87b853a7
erfannvb <erfannavab@gmail.com>
2024-04-13 18:15:17 +0330

    - hateoas is added to the saveUser method - changePassword url is changed

--- src/main/java/nvb/dev/springadvancedproject/controller/UserController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/UserController.java
@@ -2,11 +2,13 @@ package nvb.dev.springadvancedproject.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import nvb.dev.springadvancedproject.assembler.UserModelAssembler;
 import nvb.dev.springadvancedproject.dto.UserDto;
 import nvb.dev.springadvancedproject.dto.request.ChangePasswordRequest;
 import nvb.dev.springadvancedproject.mapper.UserMapper;
 import nvb.dev.springadvancedproject.model.UserEntity;
 import nvb.dev.springadvancedproject.service.UserService;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,17 +23,19 @@ public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
+    private final UserModelAssembler userModelAssembler;
 
     @PostMapping(path = "/save",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDto> saveUser(@RequestBody @Valid UserDto userDto) {
+    public ResponseEntity<EntityModel<UserDto>> saveUser(@RequestBody @Valid UserDto userDto) {
         UserEntity userEntity = userMapper.toUserEntity(userDto);
         UserEntity savedUser = userService.saveUser(userEntity);
-        return new ResponseEntity<>(userMapper.toUserDto(savedUser), HttpStatus.CREATED);
+        EntityModel<UserDto> model = userModelAssembler.toModel(savedUser);
+        return new ResponseEntity<>(model, HttpStatus.CREATED);
     }
 
-    @PatchMapping(path = "/{userId}")
+    @PatchMapping(path = "/change-password/{userId}")
     public ResponseEntity<Void> changePassword(@PathVariable String userId, @RequestBody ChangePasswordRequest request) {
         userService.changePassword(userId, request);
         return new ResponseEntity<>(HttpStatus.OK);
