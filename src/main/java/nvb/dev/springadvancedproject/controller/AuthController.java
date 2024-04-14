5d47cfbc7e50272a35346b5ac5afe595d033374d
erfannvb <erfannavab@gmail.com>
2024-04-14 20:23:28 +0330

    documented auth APIs

--- src/main/java/nvb/dev/springadvancedproject/controller/AuthController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/AuthController.java
@@ -1,5 +1,8 @@
 package nvb.dev.springadvancedproject.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import nvb.dev.springadvancedproject.security.AuthService;
 import nvb.dev.springadvancedproject.security.request.AuthRequest;
@@ -15,15 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth Controller", description = "Authenticating the Existing User")
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Generate JWT Token", description = "Generates JWT Token for the User")
+    @ApiResponse(responseCode = "200", description = "Successful Generation of JWT Token")
     @PostMapping(path = "/authenticate")
     public ResponseEntity<JwtAuthResponse> authenticate(@RequestBody AuthRequest authRequest) {
         return new ResponseEntity<>(authService.authenticate(authRequest), HttpStatus.OK);
     }
 
+    @Operation(summary = "Generate JWT Refresh Token", description = "Generates JWT Refresh Token for the User")
+    @ApiResponse(responseCode = "200", description = "Successful Generation of JWT Refresh Token")
     @PostMapping(path = "/refresh")
     public ResponseEntity<JwtAuthResponse> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
         return new ResponseEntity<>(authService.refreshToken(refreshTokenRequest), HttpStatus.OK);
