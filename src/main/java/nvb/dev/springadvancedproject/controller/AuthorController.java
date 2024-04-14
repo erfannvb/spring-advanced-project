0cd2b78570420f1fabfa61787da2428d19f8ff69
erfannvb <erfannavab@gmail.com>
2024-04-14 22:05:11 +0330

    documented author APIs

--- src/main/java/nvb/dev/springadvancedproject/controller/AuthorController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/AuthorController.java
@@ -1,5 +1,9 @@
 package nvb.dev.springadvancedproject.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import nvb.dev.springadvancedproject.dto.AuthorDto;
@@ -21,11 +25,17 @@ import java.util.stream.StreamSupport;
 @RequestMapping(path = "/api/author")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Author Controller", description = "Performing Crud Operations on Authors")
 public class AuthorController {
 
     private final AuthorService authorService;
     private final AuthorMapper authorMapper;
 
+    @Operation(summary = "Create Author", description = "Creates an Author from the provided payload")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful Creation of Author"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @PostMapping(path = "/save")
     public ResponseEntity<AuthorDto> saveAuthor(@RequestBody @Valid AuthorDto authorDto) {
         AuthorEntity authorEntity = authorMapper.toAuthorEntity(authorDto);
@@ -33,6 +43,11 @@ public class AuthorController {
         return new ResponseEntity<>(authorMapper.toAuthorDto(savedAuthor), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get author by ID", description = "Returns an author based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Retrieval of Author"),
+            @ApiResponse(responseCode = "404", description = "Author Not Found")
+    })
     @GetMapping(path = "/{id}")
     public ResponseEntity<AuthorDto> getAuthorById(@PathVariable Long id) {
         Optional<AuthorEntity> foundAuthor = authorService.getAuthorById(id);
@@ -42,6 +57,8 @@ public class AuthorController {
         }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Get all authors with page and size", description = "Provides a list of all authors")
+    @ApiResponse(responseCode = "200", description = "Successful Retrieval of Authors")
     @GetMapping(path = "/all/simple")
     public ResponseEntity<Iterable<AuthorDto>> getAllAuthors(
             @RequestParam(required = false, defaultValue = "0") int page,
@@ -53,6 +70,8 @@ public class AuthorController {
         return new ResponseEntity<>(authorDtoIterator, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get all authors with page, size and sort property", description = "Provides a list of all authors")
+    @ApiResponse(responseCode = "200", description = "Successful Retrieval of Authors")
     @GetMapping(path = "/all/bySort")
     public ResponseEntity<Iterable<AuthorDto>> getAllAuthors(
             @RequestParam(required = false, defaultValue = "0") int page,
@@ -65,6 +84,12 @@ public class AuthorController {
         return new ResponseEntity<>(authorDtoList, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update Author", description = "Update author based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Updating of Author"),
+            @ApiResponse(responseCode = "404", description = "Author Not Found"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @PutMapping(path = "/{id}")
     public ResponseEntity<AuthorDto> updateAuthor(@PathVariable Long id, @RequestBody @Valid AuthorDto authorDto) {
         AuthorEntity authorEntity = authorMapper.toAuthorEntity(authorDto);
@@ -72,18 +97,31 @@ public class AuthorController {
         return new ResponseEntity<>(authorMapper.toAuthorDto(updatedAuthor), HttpStatus.OK);
     }
 
+    @Operation(summary = "Partial Update Author", description = "Update author based on an optional field")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Partial Updating of Author"),
+            @ApiResponse(responseCode = "404", description = "Author Not Found"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @PatchMapping(path = "/{id}")
     public ResponseEntity<AuthorDto> partialUpdate(@PathVariable Long id, @RequestBody @Valid Map<String, Object> authorDto) {
         AuthorEntity authorEntity = authorService.partialUpdate(id, authorDto);
         return new ResponseEntity<>(authorMapper.toAuthorDto(authorEntity), HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete Author By ID", description = "Delete an author based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful deletion of author"),
+            @ApiResponse(responseCode = "404", description = "Author does not exist")
+    })
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<HttpStatus> deleteAuthor(@PathVariable Long id) {
         authorService.deleteAuthor(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Get Number of Authors", description = "Get Number of Saved Authors")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of authors count")
     @GetMapping(path = "/count")
     public ResponseEntity<Map<String, Long>> getNumberOfAuthors() {
         Map<String, Long> authors = new HashMap<>();
@@ -91,6 +129,11 @@ public class AuthorController {
         return new ResponseEntity<>(authors, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get Author by FirstName", description = "Get author by their first name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful retrieval of author"),
+            @ApiResponse(responseCode = "404", description = "Author not found")
+    })
     @GetMapping(path = "/filter/name")
     public ResponseEntity<AuthorDto> getAuthorByFirstName(@RequestParam String firstName) {
         Optional<AuthorEntity> authorByFirstName = authorService.getAuthorByFirstName(firstName);
@@ -100,6 +143,8 @@ public class AuthorController {
         }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Get list of authors by their age", description = "Get list of authors based on their age range")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of authors")
     @GetMapping(path = "/filter/age")
     public ResponseEntity<List<AuthorDto>> getAuthorByAgeBetween(@RequestParam int minAge, @RequestParam int maxAge) {
         List<AuthorEntity> authorByAgeBetween = authorService.getAuthorByAgeBetween(minAge, maxAge);
