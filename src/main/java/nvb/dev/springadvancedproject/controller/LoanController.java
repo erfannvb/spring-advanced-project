04613dc9cff907f6ad832065a6e70abd2105f38e
erfannvb <erfannavab@gmail.com>
2024-04-15 08:09:13 +0330

    documented loan APIs

--- src/main/java/nvb/dev/springadvancedproject/controller/LoanController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/LoanController.java
@@ -1,5 +1,9 @@
 package nvb.dev.springadvancedproject.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import nvb.dev.springadvancedproject.dto.LoanDto;
 import nvb.dev.springadvancedproject.mapper.LoanMapper;
@@ -12,11 +16,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/api/loan")
 @RequiredArgsConstructor
+@Tag(name = "Loan Controller", description = "Performing Operations on Loans")
 public class LoanController {
 
     private final LoanService loanService;
     private final LoanMapper loanMapper;
 
+    @Operation(summary = "Loan Book to Member", description = "Loans a book to the member")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful Creation of Loan"),
+            @ApiResponse(responseCode = "404", description = "Book Not Found"),
+            @ApiResponse(responseCode = "404", description = "Member Not Found"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @PostMapping(path = "/submit/member/{memberId}/book/{bookId}")
     public ResponseEntity<LoanDto> loanBookToMember(@PathVariable long memberId,
                                                     @PathVariable long bookId,
@@ -26,6 +38,13 @@ public class LoanController {
         return new ResponseEntity<>(loanMapper.toLoanDto(loanedBookToMember), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Remove Loan", description = "Discards the loan")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful Deletion of Loan"),
+            @ApiResponse(responseCode = "404", description = "Book Not Found"),
+            @ApiResponse(responseCode = "404", description = "Member Not Found"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @DeleteMapping(path = "/delete/member/{memberId}/book/{bookId}")
     public ResponseEntity<HttpStatus> deleteLoan(@PathVariable long memberId, @PathVariable long bookId) {
         loanService.deleteLoan(memberId, bookId);
