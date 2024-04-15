45adcacdd5a1c088c3f0c2cc056fd496cd33c7bd
erfannvb <erfannavab@gmail.com>
2024-04-15 08:23:07 +0330

    documented member APIs

--- src/main/java/nvb/dev/springadvancedproject/controller/MemberController.java
+++ src/main/java/nvb/dev/springadvancedproject/controller/MemberController.java
@@ -1,5 +1,9 @@
 package nvb.dev.springadvancedproject.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import nvb.dev.springadvancedproject.assembler.MemberModelAssembler;
@@ -24,6 +28,7 @@ import java.util.Optional;
 @RequestMapping(path = "/api/member")
 @RequiredArgsConstructor
 @Validated
+@Tag(name = "Member Controller", description = "Performing Crud Operations on Members")
 public class MemberController {
 
     private final MemberService memberService;
@@ -31,6 +36,11 @@ public class MemberController {
     private final MemberModelAssembler memberModelAssembler;
     private final MailService mailService;
 
+    @Operation(summary = "Create Member", description = "Creates a member from the provided payload")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful Creation of Member"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @PostMapping(path = "/save")
     public ResponseEntity<EntityModel<MemberDto>> saveMember(@RequestBody @Valid MemberDto memberDto) {
         MemberEntity memberEntity = memberMapper.toMemberEntity(memberDto);
@@ -46,6 +56,11 @@ public class MemberController {
         return new ResponseEntity<>(model, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get Member by ID", description = "Returns a member based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful Retrieval of Member"),
+            @ApiResponse(responseCode = "404", description = "Member Not Found")
+    })
     @GetMapping(path = "/{memberId}")
     public ResponseEntity<EntityModel<MemberDto>> getMemberById(@PathVariable long memberId) {
         Optional<MemberEntity> foundMember = memberService.getMemberById(memberId);
@@ -54,6 +69,11 @@ public class MemberController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Get Member by FullName", description = "Returns a member based on the full name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful Retrieval of Member"),
+            @ApiResponse(responseCode = "404", description = "Member Not Found")
+    })
     @GetMapping
     public ResponseEntity<EntityModel<MemberDto>> getMemberByFullName(@RequestParam String fullName) {
         Optional<MemberEntity> memberByFullName = memberService.getMemberByFullName(fullName);
@@ -62,6 +82,12 @@ public class MemberController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Get all the members", description = "Provides a list of all members")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Retrieval of Members"),
+            @ApiResponse(responseCode = "404", description = "Members Not Found")
+
+    })
     @GetMapping(path = "/all")
     public ResponseEntity<CollectionModel<EntityModel<MemberDto>>> getAllMembers() {
         List<MemberEntity> allMembers = memberService.getAllMembers();
@@ -72,6 +98,12 @@ public class MemberController {
         return new ResponseEntity<>(collectionModel, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update Member", description = "Update member based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Updating of Member"),
+            @ApiResponse(responseCode = "404", description = "Member Not Found"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @PutMapping(path = "/{memberId}")
     public ResponseEntity<EntityModel<MemberDto>> updateMember(@PathVariable long memberId, @RequestBody @Valid MemberDto memberDto) {
         MemberEntity memberEntity = memberMapper.toMemberEntity(memberDto);
@@ -80,6 +112,12 @@ public class MemberController {
         return new ResponseEntity<>(model, HttpStatus.OK);
     }
 
+    @Operation(summary = "Partial Update Member", description = "Partial update member based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful Updating of Member"),
+            @ApiResponse(responseCode = "404", description = "Member Not Found"),
+            @ApiResponse(responseCode = "400", description = "Bad Request : Unsuccessful Submission")
+    })
     @PatchMapping(path = "/{memberId}")
     public ResponseEntity<EntityModel<MemberDto>> partialUpdate(@PathVariable long memberId,
                                                                 @RequestBody Map<String, Object> memberDto) {
@@ -88,6 +126,11 @@ public class MemberController {
         return new ResponseEntity<>(model, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete Member by ID", description = "Delete a member based on an ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful deletion of member"),
+            @ApiResponse(responseCode = "404", description = "Member does not exist")
+    })
     @DeleteMapping(path = "/{memberId}")
     public ResponseEntity<HttpStatus> deleteMember(@PathVariable long memberId) {
         memberService.deleteMember(memberId);
